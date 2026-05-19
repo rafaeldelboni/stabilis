658c1da86fdf583f0c1019e4bcc035e358354b75
Rafael Delboni <rafadelboni@gmail.com>
2026-05-19 16:27:59 -0300

    refact: cleanup boilerplate

--- src/main.zig
+++ src/main.zig
@@ -1,21 +1,14 @@
 const std = @import("std");
 const Io = std.Io;
 
-const stabilis = @import("stabilis");
-
 const md4c = @cImport({
     @cInclude("md4c.h");
     @cInclude("md4c-html.h");
 });
 
 pub fn main(init: std.process.Init) !void {
-    // Prints to stderr, unbuffered, ignoring potential errors.
-    std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
-
-    // This is appropriate for anything that lives as long as the process.
+    // components
     const arena: std.mem.Allocator = init.arena.allocator();
-
-    // In order to do I/O operations need an `Io` instance.
     const io = init.io;
 
     const md = try readInput(arena, io, "test.md");
@@ -29,17 +22,6 @@ pub fn main(init: std.process.Init) !void {
     for (args) |arg| {
         std.log.info("arg: {s}", .{arg});
     }
-
-    // Stdout is for the actual output of your application, for example if you
-    // are implementing gzip, then only the compressed bytes should be sent to
-    // stdout, not any debugging messages.
-    var stdout_buffer: [1024]u8 = undefined;
-    var stdout_file_writer: Io.File.Writer = .init(.stdout(), io, &stdout_buffer);
-    const stdout_writer = &stdout_file_writer.interface;
-
-    try stabilis.printAnotherMessage(stdout_writer);
-
-    try stdout_writer.flush(); // Don't forget to flush!
 }
 
 fn readInput(allocator: std.mem.Allocator, io: Io, path: []const u8) ![]u8 {
@@ -74,43 +56,4 @@ fn markdownToHtml(allocator: std.mem.Allocator, md: []const u8) ![]u8 {
     if (rc != 0) return error.Md4cParseError;
     if (ctx.failed) return error.Md4cParseError;
     return ctx.list.toOwnedSlice(allocator);
-}
-
-test "simple test" {
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(i32) = .empty;
-    defer list.deinit(gpa); // Try commenting this out and see if zig detects the memory leak!
-    try list.append(gpa, 42);
-    try std.testing.expectEqual(@as(i32, 42), list.pop());
-}
-
-test "fuzz example" {
-    try std.testing.fuzz({}, testOne, .{});
-}
-
-fn testOne(context: void, smith: *std.testing.Smith) !void {
-    _ = context;
-    // Try passing `--fuzz` to `zig build test` and see if it manages to fail this test case!
-
-    const gpa = std.testing.allocator;
-    var list: std.ArrayList(u8) = .empty;
-    defer list.deinit(gpa);
-    while (!smith.eos()) switch (smith.value(enum { add_data, dup_data })) {
-        .add_data => {
-            const slice = try list.addManyAsSlice(gpa, smith.value(u4));
-            smith.bytes(slice);
-        },
-        .dup_data => {
-            if (list.items.len == 0) continue;
-            if (list.items.len > std.math.maxInt(u32)) return error.SkipZigTest;
-            const len = smith.valueRangeAtMost(u32, 1, @min(32, list.items.len));
-            const off = smith.valueRangeAtMost(u32, 0, @intCast(list.items.len - len));
-            try list.appendSlice(gpa, list.items[off..][0..len]);
-            try std.testing.expectEqualSlices(
-                u8,
-                list.items[off..][0..len],
-                list.items[list.items.len - len ..],
-            );
-        },
-    };
 }
