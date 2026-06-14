a883be16a06453cc50ea82fe4d18257ccb93a863
Rafael Delboni <rafadelboni@gmail.com>
2026-06-13 21:33:58 -0300

    feat: adds paths as args in main run

--- src/main.zig
+++ src/main.zig
@@ -32,10 +32,14 @@ pub fn main(init: std.process.Init) !void {
     const allocator = arena.allocator();
     defer arena.deinit();
 
-    const output_dir = "public";
+    var args = init.minimal.args.iterate();
+    _ = args.skip(); // skip program name
+    const output_dir = args.next() orelse "public";
+    const input_dir = args.next() orelse "example";
+
     try fs_writer.deleteDir(io, output_dir); // TODO toogle via cli
 
-    const files = try fs_reader.walkDir(io, &arena, "example/");
+    const files = try fs_reader.walkDir(io, &arena, input_dir);
     const site_data = try site.parse(&arena, files);
 
     var post_list = try std.ArrayList(Context).initCapacity(allocator, site_data.posts.len);
@@ -47,7 +51,7 @@ pub fn main(init: std.process.Init) !void {
     for (site_data.pages) |p|
         try writePage(&arena, io, output_dir, p, post_list.items, site_data);
 
-    std.debug.print("Site created on: {s}/\n", .{output_dir});
+    std.debug.print("Site from: {s}/ created on: {s}/\n", .{ input_dir, output_dir });
 }
 
 test {
